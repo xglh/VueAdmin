bb83f4915bcffd8fb00fb86589ad3e81feb6f3b1
刘辉 <hui.liu@casstime.com>
2020-05-23 18:12:03 +0800

    同步代码

--- web/src/store/modules/user.js
+++ web/src/store/modules/user.js
@@ -4,9 +4,8 @@ import router, { resetRouter } from '@/router'
 
 const state = {
   token: getToken(),
-  name: '',
+  username: '',
   avatar: '',
-  introduction: '',
   roles: []
 }
 
@@ -14,11 +13,8 @@ const mutations = {
   SET_TOKEN: (state, token) => {
     state.token = token
   },
-  SET_INTRODUCTION: (state, introduction) => {
-    state.introduction = introduction
-  },
-  SET_NAME: (state, name) => {
-    state.name = name
+  SET_USERNAME: (state, username) => {
+    state.username = username
   },
   SET_AVATAR: (state, avatar) => {
     state.avatar = avatar
@@ -36,7 +32,7 @@ const actions = {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
         commit('SET_TOKEN', data.token)
-        commit('SET_NAME', name)
+        commit('SET_USERNAME', username)
         setToken(data.token)
         resolve()
       }).catch(error => {
@@ -55,7 +51,7 @@ const actions = {
           reject('Verification failed, please Login again.')
         }
 
-        const { roles, avatar, introduction } = data
+        const { roles, avatar } = data
 
         // roles must be a non-empty array
         if (!roles || roles.length <= 0) {
@@ -64,7 +60,6 @@ const actions = {
 
         commit('SET_ROLES', roles)
         commit('SET_AVATAR', avatar)
-        commit('SET_INTRODUCTION', introduction)
         resolve(data)
       }).catch(error => {
         reject(error)
